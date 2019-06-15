d04751672b0c614be361ec745a5a6774bff5a283
Mateo Barreiros <matbarreiros@gmail.com>
2019-06-15 19:10:22 -0300

    final funcionando

---
+++ main.js
@@ -0,0 +1,29 @@
+var i = 1;
+var content = document.getElementById("row").innerHTML;
+
+function addPlayer(add) {
+
+  i > 4 ? i = 4 : "";
+  i < 1 ? i = 1 : "";
+
+  let player = "player" + i;
+  let div = document.getElementById(player);
+
+  if (add == 1) {
+    div.classList.remove("hide");
+    i++;
+  } else {
+    div.classList.add("hide");  
+    i--;
+  }
+}
+
+var app = new Vue({
+  el: "#app",
+  data: {
+    player1: 50,
+    player2: 50,
+    player3: 50,
+    player4: 50
+  }
+});
