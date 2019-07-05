33a8835be1de731c3eae54c990039d15ed841f6e
Mateo Barreiros <matbarreiros@gmail.com>
2019-07-05 13:36:01 -0300

    simpler and unintrusive update

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 var i = 2;
 var content = document.getElementById("row").innerHTML;
 
-
-window.addEventListener('load', function(e) {
-  setTimeout(function() { window.scrollTo(0, 1); }, 1);
-}, false);
+window.addEventListener(
+  "load",
+  function(e) {
+    setTimeout(function() {
+      window.scrollTo(0, 1);
+    }, 1);
+  },
+  false
+);
 
 function addPlayer(add) {
   i > 4 ? (i = 4) : "";
@@ -21,6 +26,28 @@ function addPlayer(add) {
     i--;
   }
 }
+
+Vue.component("player", {
+  template: `<div>
+  <button class="btn btn-success" v-on:click="player2 += 1">^</button>
+  <br />
+
+  <h1>
+    {{ player2 }}
+  </h1>
+
+  <h3>{{ player2name }}</h3>
+
+  <button class="btn btn-danger" v-on:click="player2 -= 1">v</button>
+  <br />
+
+  <input
+    v-model="player2name"
+    class="form-control"
+    placeholder="Name..."
+  />
+</div>`
+});
 
 var app = new Vue({
   el: "#app",
